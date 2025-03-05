77ad1141a0e28cc5d07bf8c960dc2b1f3c2bb82e
samuelwong613 <samuelwong613@gmail.com>
2025-03-05 23:53:58 +0800

    ADD  randomND function
    Added a function for generating a random number based on normal distribution.

---
+++ src/randomND.ts
@@ -0,0 +1,36 @@
+import extendProperty from './_helper';
+
+/**  
+ * Generates a random number based on mean and sd (normal distribution)
+ *
+ * @param {number} mean 		- mean for normal distribution
+ * @param {number} sd 	    - standard deviation for normal distribution
+ * @return {number} 				- a random number base on normal distribution
+ * 
+ * @example	
+ * Number.randomND(3, 1)  // might return 3.5
+ * @example	
+ * Number.randomND(3.2, 0)  // might return 3.2
+ */
+ function randomND(mean: number, sd: number): number {
+  if (typeof mean !== 'number')
+    throw new TypeError('Number.randomND parameter 1 must be a number');
+  
+  if (typeof sd !== 'number' || sd < 0)
+		throw new TypeError('Number.randomND parameter 2 must be a positive number');
+
+  return ((
+    Math.floor(Math.random()*1000)+
+    Math.floor(Math.random()*1000)+
+    Math.floor(Math.random()*1000)
+  ) / 500 - 3) * sd + mean;
+}
+
+declare global {
+  interface NumberConstructor {
+      random(size: number): number;
+      random(min: number, max: number): number;
+  }
+}
+
+extendProperty(randomND);
