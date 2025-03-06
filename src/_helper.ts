d3c797a6273928fcc3e4a743e8541f030737d52d
samuelwong613 <samuelwong613@gmail.com>
2025-03-06 22:51:31 +0800

    CHORE:  extendProperty function
    Rewrite the extendProperty function with option for collision

--- src/_helper.ts
+++ src/_helper.ts
@@ -1,18 +1,33 @@
-export default function extendProperty(func: Function, ignoreIfCollision: boolean = false, name?: string) {
-	const functionName = name ?? func.name;
+export interface Option {
+  errorIfCollided?: boolean | string,
+  warnIfCollided?: boolean | string
+  functionName?: string
+}
+
+export default function extendProperty(func: Function, option?: Option) {
+	const functionName = option?.functionName ?? func.name;
   
-  if (Number.hasOwnProperty(functionName)){
-    if (ignoreIfCollision){
-      throw Error(`Number.${functionName} has been defined. No overriding.`);
-    } else {
-      console.warn(`Number.${functionName} has been already defined. No overriding and ignore it. Please check if the behaviour is the equivalent.`);
+  if (Number.hasOwnProperty(functionName)){   
+    
+    const errorIfCollided = option?.errorIfCollided ?? true;
+    const warnIfCollided = option?.warnIfCollided ?? true;
+    const defaultErrorMessage = `Number.${functionName} has been already defined. No overriding and ignore it. Please check if the behaviour is the equivalent.`;
+    
+    // Skip defining the property
+    if (errorIfCollided){
+      console.error( errorIfCollided === true ? defaultErrorMessage : errorIfCollided );
+    } else if (warnIfCollided) {
+      console.warn( warnIfCollided === true ? defaultErrorMessage : warnIfCollided );
     }
-  }else{
+
+  } else {
+
     Object.defineProperty(Number, functionName, {
       value: func,
       writable: false,
       configurable: false,
       enumerable: false
     })
+
   }
 }
